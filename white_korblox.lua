0987917984d99bb1c61095dece1112b28662ce49
kamidrills <davesangha9@gmail.com>
2023-02-25 16:01:27 +0000

    add new files
    add white korblox and lock

---
+++ white_korblox.lua
@@ -0,0 +1,13 @@
+game:GetService("Players").PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        local character = game.Players.LocalPlayer.Character
+        character.RightFoot.Transparency = "1"
+        character.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+        character.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+        character.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+        character.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=12577535715" -- white ver
+        character.RightLowerLeg.Transparency = "1"
+        character.Head.Transparency = "1"
+        character.Head.face.Transparency = "1"
+    end)
+end)
